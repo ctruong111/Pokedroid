48d41b02e4c8a3198603cd014ffcd56c5ff51352
tonychen0110 <tonychen0110@gmail.com>
2014-12-04 18:55:22 -0800

    Revert "Fixed list being drawn under subsequent pages"
    This reverts commit 67aba85ec8e9cfba1074f936056ae69f600b54cd.
    
    Conflicts:
    	PokeDroid/bin/classes/com/example/pokedroid/MainActivity$1.class
    	PokeDroid/bin/classes/com/example/pokedroid/MainActivity.class
    	PokeDroid/res/values/strings.xml
    	PokeDroid/src/com/example/pokedroid/MainActivity.java

--- PokeDroid/gen/com/example/pokedroid/R.java
+++ PokeDroid/gen/com/example/pokedroid/R.java
@@ -9,7 +9,6 @@ package com.example.pokedroid;
 
 public final class R {
     public static final class array {
-        public static final int test=0x7f060001;
         public static final int titles=0x7f060000;
     }
     public static final class attr {
@@ -29,14 +28,12 @@ public final class R {
         public static final int drawer_shadow=0x7f020000;
         public static final int ic_drawer=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int ic_search=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f090007;
+        public static final int action_settings=0x7f090006;
         public static final int drawerLayout=0x7f090000;
         public static final int drawerList=0x7f090001;
         public static final int mainContent=0x7f090002;
-        public static final int pokemonNameList=0x7f090006;
         public static final int query=0x7f090003;
         public static final int search=0x7f090005;
         public static final int textView1=0x7f090004;
@@ -44,10 +41,9 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int generation_fragment_layout=0x7f030001;
-        public static final int home_fragment_layout=0x7f030002;
-        public static final int move_fragment_layout=0x7f030003;
-        public static final int pokemon_fragment_layout=0x7f030004;
-        public static final int type_fragment_layout=0x7f030005;
+        public static final int move_fragment_layout=0x7f030002;
+        public static final int pokemon_fragment_layout=0x7f030003;
+        public static final int type_fragment_layout=0x7f030004;
     }
     public static final class menu {
         public static final int generation_lookup=0x7f080000;
