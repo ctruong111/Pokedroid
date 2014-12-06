0864eb9f7f2dcc87451358c348d31dba5ca37a72
tonychen0110 <tonychen0110@gmail.com>
2014-12-05 21:17:35 -0800

    Fixed null pointer exception in imageTestFragment

--- PokeDroid/src/com/example/pokedroid/imageTestFragment.java
+++ PokeDroid/src/com/example/pokedroid/imageTestFragment.java
@@ -11,35 +11,13 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 
 public class imageTestFragment extends Fragment {
-	private Fragment fragment;
-	private FragmentManager manager;
-	private String type;
 
-	Button search;
-	AutoCompleteTextView query;
-	
 	public imageTestFragment() {		
 	}
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.image_test, container, false);
-		
-		//search = (Button)view.findViewById(R.id.search);
-		//query = (AutoCompleteTextView)view.findViewById(R.id.query);
-		
-		search.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				//type = query.getText().toString();
-				//
-				// IMPLEMENT TYPE DISPLAY PAGE
-				//
-				fragment = new pokemonDisplayFragment(type);
-				manager = getFragmentManager();
-				manager.beginTransaction().replace(R.id.mainContent, fragment).addToBackStack(null).commit();
-			}
-		});
 		
 		return view;
 	}
