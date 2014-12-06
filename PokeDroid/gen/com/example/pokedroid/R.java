b89b22ec65e60b0fb44c9239be7bde22edd7cbb6
Cang <cang.truong@gmail.com>
2014-12-06 13:51:07 -0800

    changed imageTest to try to list images in a list

--- PokeDroid/gen/com/example/pokedroid/R.java
+++ PokeDroid/gen/com/example/pokedroid/R.java
@@ -32,14 +32,16 @@ public final class R {
         public static final int ic_search=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f090007;
+        public static final int action_settings=0x7f090009;
         public static final int drawerLayout=0x7f090000;
         public static final int drawerList=0x7f090001;
         public static final int mainContent=0x7f090002;
+        public static final int mainInfo=0x7f090008;
         public static final int pokemonNameList=0x7f090006;
         public static final int query=0x7f090004;
         public static final int recents=0x7f090003;
         public static final int search=0x7f090005;
+        public static final int textView1=0x7f090007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -47,12 +49,15 @@ public final class R {
         public static final int generation_fragment_layout=0x7f030002;
         public static final int home_fragment_layout=0x7f030003;
         public static final int image_test=0x7f030004;
-        public static final int move_display_fragment_layout=0x7f030005;
-        public static final int move_fragment_layout=0x7f030006;
-        public static final int pokemon_display_fragment_layout=0x7f030007;
-        public static final int pokemon_fragment_layout=0x7f030008;
-        public static final int type_display_fragment_layout=0x7f030009;
-        public static final int type_fragment_layout=0x7f03000a;
+        public static final int location_fragment_layout=0x7f030005;
+        public static final int main_info_fragment_layout=0x7f030006;
+        public static final int move_display_fragment_layout=0x7f030007;
+        public static final int move_fragment_layout=0x7f030008;
+        public static final int move_search_fragment_layout=0x7f030009;
+        public static final int pokemon_fragment_layout=0x7f03000a;
+        public static final int pokemon_main_info_layout=0x7f03000b;
+        public static final int type_display_fragment_layout=0x7f03000c;
+        public static final int type_fragment_layout=0x7f03000d;
     }
     public static final class menu {
         public static final int generation_lookup=0x7f080000;
--- PokeDroid/src/com/example/pokedroid/DatabaseHelper.java
+++ PokeDroid/src/com/example/pokedroid/DatabaseHelper.java
@@ -1,12 +1,18 @@
 package com.example.pokedroid;
 
+import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.util.ByteArrayBuffer;
+
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
@@ -14,30 +20,31 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteStatement;
 import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 	public static String DB_PATH = "/data/data/com.example.pokedroid/databases/";
 	public static String DB_NAME = "Pokemon";
 	public static final int DB_VERSION = 1;
-	
+
 	private SQLiteDatabase myDB;
 	private Context context;
-	
+
 	public DatabaseHelper(Context context) {
 		super(context, DB_NAME, null, DB_VERSION);
 		// TODO Auto-generated constructor stub
 		this.context = context;
 	}
-	
+
 	@Override
 	public SQLiteDatabase getWritableDatabase() throws SQLException {
-        //Open the database
-        String myPath = DB_PATH + DB_NAME;
-        myDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
-        
-        return myDB;
-    }
+		//Open the database
+		String myPath = DB_PATH + DB_NAME;
+		myDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
+
+		return myDB;
+	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
@@ -48,81 +55,81 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO Auto-generated method stub		
 	}
-	
+
 	@Override
 	public synchronized void close(){
-	    if(myDB!=null){
-	        myDB.close();
-	    }
-	    super.close();
+		if(myDB!=null){
+			myDB.close();
+		}
+		super.close();
 	}
-	
+
 	/*
 	 * Check if the database is exist on device or not
 	 */
 	private boolean checkDataBase() {
-	    SQLiteDatabase tempDB = null;
-	    try {
-	        String myPath = DB_PATH + DB_NAME;
-	        tempDB = SQLiteDatabase.openDatabase(myPath, null,
-	                SQLiteDatabase.OPEN_READWRITE);
-	    } catch (SQLiteException e) {
-	        Log.e("tle99 - check", e.getMessage());
-	    }
-	    if (tempDB != null)
-	        tempDB.close();
-	    return tempDB != null ? true : false;
+		SQLiteDatabase tempDB = null;
+		try {
+			String myPath = DB_PATH + DB_NAME;
+			tempDB = SQLiteDatabase.openDatabase(myPath, null,
+					SQLiteDatabase.OPEN_READWRITE);
+		} catch (SQLiteException e) {
+			Log.e("tle99 - check", e.getMessage());
+		}
+		if (tempDB != null)
+			tempDB.close();
+		return tempDB != null ? true : false;
 	}
-	
+
 	/*
 	 * Copy database from source code assets to device
 	 * @throws IOException
 	 */
 	public void copyDataBase() throws IOException{
-	    try {
-	        InputStream myInput = context.getAssets().open(DB_NAME);
-	        String outputFileName = DB_PATH + DB_NAME;
-	        OutputStream myOutput = new FileOutputStream(outputFileName);
+		try {
+			InputStream myInput = context.getAssets().open(DB_NAME);
+			String outputFileName = DB_PATH + DB_NAME;
+			OutputStream myOutput = new FileOutputStream(outputFileName);
 
-	        byte[] buffer = new byte[1024];
-	        int length;
+			byte[] buffer = new byte[1024];
+			int length;
 
-	        while((length = myInput.read(buffer))>0){
-	            myOutput.write(buffer, 0, length);
-	        }
+			while((length = myInput.read(buffer))>0){
+				myOutput.write(buffer, 0, length);
+			}
 
-	        myOutput.flush();
-	        myOutput.close();
-	        myInput.close();
-	    } catch (Exception e) {
-	        Log.e("tle99 - copyDatabase", e.getMessage());
-	    }
+			myOutput.flush();
+			myOutput.close();
+			myInput.close();
+		} catch (Exception e) {
+			Log.e("tle99 - copyDatabase", e.getMessage());
+		}
 	}
-	
+
 	public void openDataBase() throws SQLException{
-	    String myPath = DB_PATH + DB_NAME;
-	    myDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
+		String myPath = DB_PATH + DB_NAME;
+		myDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
 	}
-	
+
 	/***
 	 * Check if the database doesn't exist on device, create new one
 	 * @throws IOException
 	 */
 	public void createDataBase() throws IOException {
-	    boolean dbExist = checkDataBase();        
+		boolean dbExist = checkDataBase();        
 
-	    if (dbExist) {
+		if (dbExist) {
 
-	    } else {
-	        this.getReadableDatabase();
-	        try {
-	            copyDataBase();
-	        } catch (IOException e) {
-	            Log.e("tle99 - create", e.getMessage());
-	        }
-	    }
+		} else {
+			this.getReadableDatabase();
+			try {
+				copyDataBase();
+			} catch (IOException e) {
+				Log.e("tle99 - create", e.getMessage());
+			}
+		}
 	}
-	
+
 	public List<String> getAllTypeNames() {
 		List<String> typeNames = new ArrayList<String>();
 		SQLiteDatabase db = this.getWritableDatabase();
@@ -134,24 +141,24 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 			if (cursor == null) {
 				return null;
 			} 
-			
+
 			cursor.moveToFirst();
-			
+
 			do {
-	            name = cursor.getString(1);            
-	            typeNames.add(name);
-	        } while (cursor.moveToNext()); 
-			
-	        cursor.close();
-			
+				name = cursor.getString(1);            
+				typeNames.add(name);
+			} while (cursor.moveToNext()); 
+
+			cursor.close();
+
 		} catch (Exception e) {
 			Log.e("tle99", e.getMessage());
 		}
-		
+
 		db.close();
 		return typeNames;
 	}
-	
+
 	public List<String> getAllPokemonNamesAndId() {
 		List<String> pokemonNames = new ArrayList<String>();
 		SQLiteDatabase db = this.getWritableDatabase();
@@ -165,34 +172,34 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 			if (cursor == null) {
 				return null;
 			} 
-			
+
 			cursor.moveToFirst();
-			
+
 			do {
 				id = cursor.getInt(0);
-	            name = cursor.getString(1);
-	            
-	            //Capitalizes first letter
+				name = cursor.getString(1);
+
+				//Capitalizes first letter
 				String output = name.substring(0, 1).toUpperCase() + name.substring(1);
 				name = output;
-	            
+
 				//Adds in pokemon number with leading zeroes before name
-	            temp = String.format("%03d", id);
-	            temp = temp + "\t\t" + name;
-	            
-	            pokemonNames.add(temp);
-	        } while (cursor.moveToNext()); 
-			
-	        cursor.close();
-			
+				temp = String.format("%03d", id);
+				temp = temp + "\t\t" + name;
+
+				pokemonNames.add(temp);
+			} while (cursor.moveToNext()); 
+
+			cursor.close();
+
 		} catch (Exception e) {
 			Log.e("tle99", e.getMessage());
 		}
-		
+
 		db.close();
 		return pokemonNames;
 	}
-	
+
 	public List<String> getAllPokemonNames() {
 		List<String> pokemonNames = new ArrayList<String>();
 		SQLiteDatabase db = this.getWritableDatabase();
@@ -206,30 +213,30 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 			if (cursor == null) {
 				return null;
 			} 
-			
+
 			cursor.moveToFirst();
-			
+
 			do {
 				id = cursor.getInt(0);
-	            name = cursor.getString(1);
-	            
-	            //Capitalizes first letter
+				name = cursor.getString(1);
+
+				//Capitalizes first letter
 				String output = name.substring(0, 1).toUpperCase() + name.substring(1);
 				name = output;
 
-	            pokemonNames.add(name);
-	        } while (cursor.moveToNext()); 
-			
-	        cursor.close();
-			
+				pokemonNames.add(name);
+			} while (cursor.moveToNext()); 
+
+			cursor.close();
+
 		} catch (Exception e) {
 			Log.e("tle99", e.getMessage());
 		}
-		
+
 		db.close();
 		return pokemonNames;
 	}
-	
+
 	public Pokemon getPokemon(String name) {
 		Pokemon pokemon = new Pokemon(name);
 		SQLiteDatabase db = this.getWritableDatabase();
@@ -241,11 +248,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 					+ "JOIN TP ON pokemon = p_id "
 					+ "JOIN Type ON type = t_id "
 					+ "WHERE p_name = '" + name + "' COLLATE NOCASE", null);
-			
+
 			if (cursor == null) {
 				return null;
 			} cursor.moveToFirst();
-			
+
 			pokemon.setId(cursor.getInt(0));
 			pokemon.setHeight(cursor.getDouble(2)/10);
 			pokemon.setWeight(cursor.getDouble(3)/10);
@@ -257,22 +264,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 			pokemon.setSpecial_defence(cursor.getInt(10));
 			pokemon.setSpeed(cursor.getInt(11));
 			pokemon.setType1(cursor.getString(15));
-			
+
 			if (cursor.getString(16) == null) {
 				pokemon.setType2(cursor.getString(16));
 			} else {
 				pokemon.setType2("-");
 			}
-			
+
 			cursor.close();
-			
+
 		} catch (Exception e) {
 			Log.e("tle99", e.getMessage());
 		}
 		db.close();
 		return pokemon;
 	}
-	
+
 	public List<String> getAllMoveNames() {
 		List<String> moveNames = new ArrayList<String>();
 		SQLiteDatabase db = this.getWritableDatabase();
@@ -284,20 +291,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 			if (cursor == null) {
 				return null;
 			} 
-			
+
 			cursor.moveToFirst();
-			
+
 			do {
-	            name = cursor.getString(1);            
-	            moveNames.add(name);
-	        } while (cursor.moveToNext()); 
-			
-	        cursor.close();
-			
+				name = cursor.getString(1);            
+				moveNames.add(name);
+			} while (cursor.moveToNext()); 
+
+			cursor.close();
+
 		} catch (Exception e) {
 			Log.e("tle99", e.getMessage());
 		}
-		
+
 		db.close();
 		return moveNames;
 	}
@@ -305,19 +312,82 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 		Move move = new Move();
 		SQLiteDatabase db = this.getWritableDatabase();
 		Cursor cursor;
-		
+
 		try {
 			cursor = db.rawQuery("SELECT * FROM Moves JOIN Typen ON t_id = type WHERE m_name = '" + name + "' COLLATE NOCASE", null);
 
 			if (cursor == null) {
 				return null;
 			} cursor.moveToFirst();
-			
+
 		} catch (Exception e) {
 			Log.e("tle99", e.getMessage());
 		}
-		
+
 		db.close();
 		return move;
+	}
+
+	private byte[] getImage(String url){ //helper method
+		try {
+			URL imageUrl = new URL(url);
+			URLConnection ucon = imageUrl.openConnection();
+
+			InputStream is = ucon.getInputStream();
+			BufferedInputStream bis = new BufferedInputStream(is);
+
+			ByteArrayBuffer baf = new ByteArrayBuffer(500);
+			int current = 0;
+			while ((current = bis.read()) != -1) {
+				baf.append((byte) current);
+			}
+
+			return baf.toByteArray();
+		} catch (Exception e) {
+			Log.d("ImageManager", "Error: " + e.toString());
+		}
+		return null;
+	}
+
+	public void insertImage(byte[] img, String name){ //helper method
+		SQLiteDatabase db               =   getWritableDatabase();
+		ContentValues newValues = new ContentValues();
+		newValues.put("image", img);
+		db.update("Pokemon", newValues, "p_name = " + name, null);
+		db.close();
+	}
+
+	public List<byte[]> getImageList() {
+	    SQLiteDatabase db       =   getWritableDatabase();
+	    Cursor cursor;
+	    List<byte[]> list = new ArrayList<byte[]>();
+
+	    try {
+			cursor = db.rawQuery("SELECT image FROM Pokemon", null);
+			if (cursor == null) {
+				return null;
+			} 
+
+			cursor.moveToFirst();
+
+			do {
+				byte[] temp = cursor.getBlob(0);            
+				list.add(temp);
+			} while (cursor.moveToNext()); 
+
+			cursor.close();
+
+		} catch (Exception e) {
+			Log.e("tle99", e.getMessage());
+		}
+	    return list;
+	}
+	
+	public void insertImages(){ //insert all images *hopefully*
+		List<String> names = getAllPokemonNames();
+		for(int i = 0; i < names.size(); i++){
+			byte[] entry = getImage("http://img.pokemondb.net/artwork/"+ names.get(i) +".jpg");
+			insertImage(entry, names.get(i));
+		}
 	}
 }
