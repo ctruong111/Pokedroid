57694259e80b6676bee4e2463003d4fe6bc133a8
Cang <cang.truong@gmail.com>
2014-12-07 19:39:44 -0800

    changed abilites to try to get it to work.

--- PokeDroid/src/com/example/pokedroid/Abilities.java
+++ PokeDroid/src/com/example/pokedroid/Abilities.java
@@ -4,13 +4,13 @@ public class Abilities {
 	private static String name;
 	private static String description;
 	
-	public static String getName() {
+	public  String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public static String getDescription() {
+	public  String getDescription() {
 		return description;
 	}
 	public void setDescription(String description) {
